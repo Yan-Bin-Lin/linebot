bafe7f7ac25b9245d1e2acf19afdad4ceacadb96
Danny Lin <beyond1generation@gmail.com>
2017-11-24 21:26:03 +0800

    Eddy Green!

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*- 
 '''
     test flask framework
     by yan-bin-lin
 '''
+
 from flask import Flask
 import os
 from flask import request
@@ -15,27 +17,32 @@ def website_test():
     return '<p>This is a flask test</p>'
 
 @app.route('/callback', methods=['POST'])
+#first step
 def callback():
+    #get json input
     decode = request.get_json()
-    channel_token = "w5LR/GWzxSwwVP910AG4AOaDBv0Ys7bEW2yJM9qYdyBfHhgnh1mRJlXKLOpncI/f5iEJLb38bLWriV9AoZ72p45BODmeil/Ux7iWSbYqgcFx9E1uLwf1kCWk6luXUQUH0ZN5WxhRITHYZjx5balb0AdB04t89/1O/w1cDnyilFU="
-    reply_token = decode['events'][0]["replyToken"]
-    print(reply_token)
-    line_bot_api = LineBotApi(channel_token)
-    line_bot_api.reply_message(reply_token, TextSendMessage(text='Hello World!'))
-    return "<p>hello world</p>"
     
-'''    
-    file = request.get_json()
-    decode = json.load(file)
-    line_type = decode['events'][0]['type']
-    text = decode['events'][0]['message']['text']
-    judge = text[0:8]
-    strlen = len(text)
-    if judge == "linebot:":
-        control = text[9:strlen]
-        return "<p>" + control + "</p>"
-    return ""
-'''
+    if decode['events'][0]['type'] == "message":
+        #start line_bot_api
+        channel_token = "w5LR/GWzxSwwVP910AG4AOaDBv0Ys7bEW2yJM9qYdyBfHhgnh1mRJlXKLOpncI/f5iEJLb38bLWriV9AoZ72p45BODmeil/Ux7iWSbYqgcFx9E1uLwf1kCWk6luXUQUH0ZN5WxhRITHYZjx5balb0AdB04t89/1O/w1cDnyilFU="
+        line_bot_api = LineBotApi(channel_token)
+        #get reply token
+        reply_token = decode['events'][0]["replyToken"]
+        #get message
+        text = decode['events'][0]['message']['text']
+        #example message:"lb. news 1"
+        tlist = text.split(' ')
+        if len(tlist) >= 2:
+            judge = tlist[0] #lb.
+            method = tlist[1] #news
+            if len(tlist) == 3:
+                control = tlist[2] #1        
+                if judge == "lb.":
+                    if method == "eddy":
+                        line_bot_api.reply_message(reply_token, TextSendMessage(text='Eddy Green!'))
+                    else:
+                        line_bot_api.reply_message(reply_token, TextSendMessage(text = 'µL®Ä«ü¥O:' + method ))           
+    return "<p>hello world</p>"
 
 
 
